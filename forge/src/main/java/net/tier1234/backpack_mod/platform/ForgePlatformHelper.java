1a841649a013935435ed2008754d2ad97f95a546
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-03-15 17:24:39 +0100

    Initial commit

---
+++ forge/src/main/java/net/tier1234/backpack_mod/platform/ForgePlatformHelper.java
@@ -0,0 +1,23 @@
+package net.tier1234.backpack_mod.platform;
+
+import net.tier1234.backpack_mod.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+        return !FMLLoader.isProduction();
+    }
+}
